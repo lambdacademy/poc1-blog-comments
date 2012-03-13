d480c01011d8d062983b8c95b67e41e29b215e10
Greg Belote <etoleb@gmail.com>
2012-03-13 15:43:24 -0400

    updated checks

--- .lambda/boot.rb
+++ .lambda/boot.rb
@@ -1,8 +1,20 @@
+puts "HIYA"
+require "rubygems"
+require "bundler/setup"
+
 require 'sinatra'
 
 set :port, 22785
 
 get '/' do
     "Success!"
+end
+
+get 'alive' do
+    true
+end
+
+get 'bundle/check' do
+    system("bundle check")
 end
 
